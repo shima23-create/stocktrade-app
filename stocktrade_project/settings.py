66aa24d4facd91b2e28341169ee57e576c5312e3
shima <utm2577223@stu.o-hara.ac.jp>
2026-06-01 23:24:29 +0900

    PostgreSQLに移行

--- stocktrade_project/settings.py
+++ stocktrade_project/settings.py
@@ -71,11 +71,14 @@ WSGI_APPLICATION = 'stocktrade_project.wsgi.application'
 
 
 # Database
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
